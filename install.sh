873ca87b6c03dedf23affeb4afcd2a7ea1ff41e0
iamwwc <iam.wuweichao@gmail.com>
2019-09-04 19:18:27 -0700

    add install sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+red='\033[0;31m'
+plain='\033[0m'
+
+[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] This script must be run as root!" && exit 1
+
+# download wwcdocker binary
+curl -s https://api.github.com/repos/iamwwc/wwcdocker/releases/latest \
+  | grep browser_download_url \
+  | cut -d '"' -f 4 \
+  | wget -i -
+
+cwd=$(pwd)
+chmod u+x wwcdocker
+#export to path
+export PATH=$PATH:${cwd}
+
+green='\033[0;32m'
+echo -e "${green}Type wwcdocker for help :)${plain}"
